5cce3e731866e09c9bd995373b9bc18fa5206431
Artem Mikhaylov <m.av1922@yandex.ru>
2025-04-27 18:38:31 +0300

    add doc detail view adn doc row

---
+++ LegalAI Summarizer/View/DocumentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentDetailView.swift
+//  LegalAI Summarizer
+//
+//  Created by Артем Михайлов on 27.04.2025.
+//
+
+import SwiftUI
+
+struct DocumentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentDetailView()
+}
---
+++ LegalAI Summarizer/View/DocumentRow.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentRow.swift
+//  LegalAI Summarizer
+//
+//  Created by Артем Михайлов on 27.04.2025.
+//
+
+import SwiftUI
+
+struct DocumentRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentRow()
+}
