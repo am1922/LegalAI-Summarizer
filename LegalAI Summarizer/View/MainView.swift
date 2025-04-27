c85bd422db4078de410cdbcd3587d3d9e7f6c590
Artem Mikhaylov <m.av1922@yandex.ru>
2025-04-27 18:34:36 +0300

    replace some view

---
+++ LegalAI Summarizer/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  LegalAI Summarizer
+//
+//  Created by Артем Михайлов on 27.04.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
