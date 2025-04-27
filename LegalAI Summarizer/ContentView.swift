d374c5995cc30cc793ff56ada6dcea91b1ba2c20
Artem Mikhaylov <m.av1922@yandex.ru>
2025-04-27 18:28:47 +0300

    add tab bar

--- LegalAI Summarizer/ContentView.swift
+++ LegalAI Summarizer/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+
+            MainView()
+                .tabItem {
+                    Label("Главная", systemImage: "house.fill")
+                }
+            
+            SearchView()
+                .tabItem {
+                    Label("Поиск", systemImage: "magnifyingglass")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Профиль", systemImage: "person.crop.circle")
+                }
         }
-        .padding()
+        .accentColor(.blue)
     }
 }
 
