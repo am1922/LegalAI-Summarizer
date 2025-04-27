98baf74f800cd78b31ed9fc69d91d14a3d26b396
Artem Mikhaylov <m.av1922@yandex.ru>
2025-04-27 18:24:07 +0300

    Initial Commit

---
+++ LegalAI Summarizer/LegalAI_SummarizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegalAI_SummarizerApp.swift
+//  LegalAI Summarizer
+//
+//  Created by Артем Михайлов on 27.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LegalAI_SummarizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
