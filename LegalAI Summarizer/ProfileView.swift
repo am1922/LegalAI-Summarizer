6ac939414e664a12b4572a51c471dd34e3ac1eee
Artem Mikhaylov <m.av1922@yandex.ru>
2025-04-27 18:31:51 +0300

    add some view

---
+++ LegalAI Summarizer/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  LegalAI Summarizer
+//
+//  Created by Артем Михайлов on 27.04.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ LegalAI Summarizer/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  LegalAI Summarizer
+//
+//  Created by Артем Михайлов on 27.04.2025.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
